1fd6abcbfa050b796e03679d4f8d0fd9bdd64aed
BSI85 <120641466+BSI85@users.noreply.github.com>
2023-09-19 03:07:37 +0500

    added scroll

--- js/script.js
+++ js/script.js
@@ -1,10 +1,40 @@
-$(document).ready(function () {
-  //burger
-  $('.header-menu__icon').click(function (e) {
-    $('.header-menu__icon, .header-menu').toggleClass('_active');
-    $('body').toggleClass('_lock');
+const iconMenu = document.querySelector('.header-menu__icon');
+const menuBody = document.querySelector('.header-menu');
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
   });
-});
+}
+
+const menuLinks = document.querySelectorAll('.header-bottom-menu__link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+      if (iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+      }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+      e.preventDefault();
+    }
+  }
+}
 
 $(window).resize(function (e) {
   adaptive_function();
